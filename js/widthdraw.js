059fb8751ea969b3a03242c719ad6ce6e0e1cb0f
mirza-mohibul-hasan <mirzamohibul618@gmail.com>
2023-02-14 19:34:12 +0600

    withdraw and Nan bug fixed

--- js/widthdraw.js
+++ js/widthdraw.js
@@ -1,20 +1,26 @@
-document.getElementById('btn-withdraw').addEventListener('click',function(){
+document.getElementById('btn-withdraw').addEventListener('click', function () {
     //get input withdrawl amount
     const withdrawAmountField = document.getElementById('withdraw-amount');
     const withdrawAmount = withdrawAmountField.value;
+    document.getElementById('withdraw-amount').value = '';
+    if(isNaN(parseFloat(withdrawAmount))){
+        alert('Please provide valid number');
+        return;
+    }
 
     //get previous widthdrawl
     const previousWithdrawElement = document.getElementById('withdraw-total');
     const previousWithdraw = previousWithdrawElement.innerText;
 
-    //set widthdraw total
-    document.getElementById('withdraw-total').innerText = parseFloat(previousWithdraw)+parseFloat(withdrawAmount);
-
     //get previous total balance
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotal = balanceTotalElement.innerText;
-    //set total blance
-    document.getElementById('balance-total').innerText = parseFloat(previousBalanceTotal) - parseFloat(withdrawAmount);
-
-    document.getElementById('withdraw-amount').value = '';
+    //set total balance && set widthdraw total
+    if (parseFloat(previousBalanceTotal) >= parseFloat(withdrawAmount)) {
+        document.getElementById('withdraw-total').innerText = parseFloat(previousWithdraw) + parseFloat(withdrawAmount);
+        document.getElementById('balance-total').innerText = parseFloat(previousBalanceTotal) - parseFloat(withdrawAmount);
+    }
+    else{
+        alert('Baap er bank a oto taka nai');
+    }
 })
